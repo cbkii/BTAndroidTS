0a7ebbc7777306b55874ce1694b6629977a70256
tuuhin <tuhinbhowmick2513@gmail.com>
2026-02-07 20:31:52 +0530

    updated version in build.gradle.kts and removed deprecated  method in LightSensorReaderImpl.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
 		applicationId = "com.eva.bluetoothterminalapp"
 		minSdk = 29
 		targetSdk = 36
-		versionCode = 4
-		versionName = "1.2.0"
+		versionCode = 5
+		versionName = "1.2.1"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables {
